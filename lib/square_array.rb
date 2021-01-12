b63644b7f6b5835792e75b4a633702f8d99f6466
Liz Burke <liz.burke.chi@gmail.com>
2021-01-12 22:46:53 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,10 +1,11 @@
-
+require 'pry'
 
 def square_array(array)
   count = 0
   new_array = []
   while count <= array.length do
     new_array.push(array[count]**2)
+    binding.pry
   count += 1
   end
   new_array
